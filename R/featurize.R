bfeaa94d1ce4a178aa27d47712507ca155590cfa
Asha Anoosheh <ashaanoosheh@aol.com>
2016-04-22 23:44:10 -0700

    Create multicore featurization: up to 4x faster now

--- R/featurize.R
+++ R/featurize.R
@@ -18,7 +18,9 @@ rm(db)
 # Subset to 1% to reduce computation time during development.
 # Create features; raw feature processing is especially slow and needs to be optimized.
 # NOTE: this returns a list, where $data contains the actual dataframe.
+ptm <- proc.time()
 data_processed = featurize(raw_data, downsample_pct = 0.01)
+proc.time() - ptm
 
 # Save our results.
 save(data_processed, file="data/data-featurized.RData")
